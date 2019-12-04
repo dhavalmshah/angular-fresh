823d2e1400f1357675e21c6e654138c003656e2d
dhavalmshah <dhavalmshah@gmail.com>
2019-12-04 07:16:36 +0000

    Added new changes

--- initapp.sh
+++ initapp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 npm install --global yo generator-angular-fullstack gulp-cli
 npm i -S babel-register
+npm install lodash --save
+npm install @babel/core --save
+npm install @babel/register --save
 yo angular-fullstack
